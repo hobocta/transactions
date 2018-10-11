8240803ffa3d451bc6d572d22ee952ef6b27ebb4
Антон Кондратов <hobocta@gmail.com>
2018-10-12 00:41:22 +0300

    Рефакторинг

--- migrations/004_add_user_and_balance_rows_to_tables.php
+++ migrations/004_add_user_and_balance_rows_to_tables.php
@@ -12,27 +12,45 @@ require __DIR__ . '/../vendor/autoload.php';
 
 try {
     $application = new Application();
-    /** @noinspection SqlResolve */
-    $pdoStatement = $application->database->pdo->prepare('
-        INSERT INTO `users` (`login`, `password_hash`)
-        VALUES (:login, :password_hash);
-    ');
-    $pdoStatement->execute([
-        ':login' => 'admin',
-        ':password_hash' => password_hash('admin', PASSWORD_DEFAULT),
-    ]);
-    $userId = $application->database->pdo->lastInsertId();
 
-    /** @noinspection SqlResolve */
-    $pdoStatement = $application->database->pdo->prepare('
-        INSERT INTO `balance` (`user_id`, `balance`)
-        VALUES (:user_id, :balance);
-    ');
-    $pdoStatement->execute([
-        ':user_id' => $userId,
-        ':balance' => 999999999,
-    ]);
-    echo 'Done' . PHP_EOL;
+    $application->database->startTransaction();
+
+    try {
+        /** @noinspection SqlResolve */
+        $result = $application->database->pdo->prepare('
+            INSERT INTO `users` (`login`, `password_hash`)
+            VALUES (:login, :password_hash);
+        ')->execute([
+            ':login' => 'admin',
+            ':password_hash' => password_hash('admin', PASSWORD_DEFAULT),
+        ]);
+        if (!$result) {
+            throw new CommonException('Execute failed');
+        }
+        $userId = $application->database->pdo->lastInsertId();
+        if (!$userId) {
+            throw new CommonException('Unable to get lastInsertId');
+        }
+
+        /** @noinspection SqlResolve */
+        $result = $application->database->pdo->prepare('
+            INSERT INTO `balance` (`user_id`, `balance`)
+            VALUES (:user_id, :balance);
+        ')->execute([
+            ':user_id' => $userId,
+            ':balance' => 999999999,
+        ]);
+        if (!$result) {
+            throw new CommonException('Execute failed');
+        }
+
+        $application->database->commit();
+
+        echo 'Done' . PHP_EOL;
+    } catch (CommonException $e) {
+        $application->database->rollback();
+        throw $e;
+    }
 } catch (CommonException $e) {
     ExceptionLogger::log($e);
     die(sprintf('Exception message: %s (%s:%s)', $e->getMessage(), $e->getFile(), $e->getLine()));
